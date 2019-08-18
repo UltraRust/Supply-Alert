e6cbfeef92f11a139216835fff3e3c4174ddd379
UltraRust <53350204+UltraRust@users.noreply.github.com>
2019-08-18 23:12:12 +0200

    Add files via upload

---
+++ SupplyAlert.cs
@@ -0,0 +1,33 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Supply Alert", "Ultra", "1.1.2")]
+    [Description("Sound or visual alert for dropped supply")]
+
+    class SupplyAlert : RustPlugin
+    {
+        [ChatCommand("g")]
+        void RunEffect(BasePlayer player)
+        {
+            Alert();
+        }
+
+        void OnEntitySpawned(BaseEntity baseEntity)
+        {
+
+            if (baseEntity != null && baseEntity is SupplyDrop)
+            {
+                Alert();
+            }
+        }
+       
+        void Alert()
+        {
+            foreach (var player in BasePlayer.activePlayerList)
+            {
+                SendReply(player, string.Format("<color=#ee2211>{0}</color>", "+++ Supply has dropped +++"));
+                Effect.server.Run("assets/bundled/prefabs/fx/invite_notice.prefab", player.transform.position);
+            }
+
+        }
+    }
+}
